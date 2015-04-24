64168fc6c59869733a64c543a6b2c77970908f08
Nephi <lupus590@users.noreply.github.com>
2015-04-24 14:22:12 +0100

    Manual restore, copy of master except for travis and make

---
+++ assets/music/README.md
@@ -0,0 +1,2 @@
+music source
+[pacman.mp3] (http://www.classicgaming.cc/classics/pacman/sounds.php)
---
+++ src/test.cpp
@@ -0,0 +1,12 @@
+#include "test.h"
+
+int main(void)
+{
+	string test;
+	cout<<"It works!\n";
+        //cout<<"DB breakpoint test\n";
+	cin>>test;
+        //return 1;
+        return 0;
+}
+
