28720bea4d2024c782050e109332c68f73a15d20
James <Cook-E@users.noreply.github.com>
2015-04-28 06:31:24 +0100

    Update Player.cpp

--- src/Player.cpp
+++ src/Player.cpp
@@ -1,77 +1,57 @@
+/* SEP ASSIGNMENT 2
+Programmed by James Cookson */
+
 #include "Player.h"
 
 Player::Player()
 {
-	//startPoint.x = 24;
-	//startPoint.y = 14;
-
-	Node N;
-	Level L;
-
-	for (int i = 0; i < L.row; i++)
-	{
-		for (int j = 0; j < L.col; j++)
-		{
-			if (L.Maze[i][j].value == 2)
-			{
-				L.Maze[startPoint.x][startPoint.y] = L.Maze[i][j];
-			}
-		}
-	}
-
-	//myMove = NONE;
 }
 
 Player::~Player()
 {
 }
 
-void Player::setMove(enum move m)
-{
-	//myMove = m;
-}
-/*
-void Player::Direction()
-{
-	//Checks all four directions to decide best path to take
-	Check(1, 0); //Check right
-	Check(-1, 0); //Check left
-	Check(0, 1); //Check down
-	Check(0, -1); //Check up
-}
-*/
-
 void Player::Check(int a, int b)
 {
-	
-	if (F.Maze[startPoint.x + a][startPoint.y + b].value == 0 /*&& F.Maze[startPoint.x + a][startPoint.y + b].visited == 0*/) //If the next node is empty and unvisited
+	if (L.Maze[L.startPoint.x + a][L.startPoint.y + b].value == 0) //If the area is an open space
 	{
-		//Node temp = F.Maze[startPoint.x + a][startPoint.y + b];
-		//F.Maze[startPoint.x][startPoint.y] = F.Maze[startPoint.x + a][startPoint.y + b];
-		//F.Maze[startPoint.x][startPoint.y] = temp;
+		//Temporary storage of player co-ordinates
+		int tempX = L.startPoint.x;
+		int tempY = L.startPoint.y;
+		
+		//Move player to new co-ordinates
+		L.startPoint.x += a;
+		L.startPoint.y += b;
+		
+		for (int i = 0; i < L.row + 1; i++)
+		{
+			for (int j = 0; j < L.col + 1; j++)
+			{
+				if (i == tempX && j == tempY)
+				{
+					L.Maze[i][j].value = 0; //Set value of old position to 0 (Open space)
+				}
+				else
+				{
+					if (i == L.startPoint.x && j == L.startPoint.y)
+					{
+						L.Maze[i][j].value = 2; //Set value of new position to 2 (Player Occupied)
+					}
+				}
+			}
+		}
 
-		F.Maze[startPoint.x][startPoint.y] = F.Maze[startPoint.x + a][startPoint.y + b];
+		//Store player co-ordinates inside local variable
+		playerPoint.x = L.startPoint.x;
+		playerPoint.y = L.startPoint.y;
 
-		//F.Maze[startPoint.x][startPoint.y].value = 2;
-		/*
-		startPoint.x = startPoint.x + a; //Check left/right
-		startPoint.y = startPoint.y + b; //Check up/down
-		*/
-		// F.Maze[startPoint.x][startPoint.y].visited = 1; //Mark the node as visited
-
-		//Direction(); //Recursion, check for next move
-
-		//Return variables to initial values
-		/*
-		length--;
-		F.Maze[startPoint.x][startPoint.y].visited = 0;
-		startPoint.x = startPoint.x - a;
-		startPoint.y = startPoint.y - b;
-		*/
+		cout << "Player Movement: " << endl;
+		L.PrintMaze(); //Prints out maze with new player position
 	}
-	/*
 	
-F.Maze[startPoint.x][startPoint.y] = F.Maze[startPoint.x + a][startPoint.y + b];
-F.Maze[startPoint.x][startPoint.y].value = 2;
-*/
+	else
+	{
+		cout << "Player Movement: " << endl;
+		L.PrintMaze(); //Prints out maze with unadjusted player position
+	}
 }
