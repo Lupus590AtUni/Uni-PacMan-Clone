5db1b00eb7635a67bb0a8d407b97f3f79712158e
James <Cook-E@users.noreply.github.com>
2015-04-28 06:31:53 +0100

    Update Player.h

--- src/Player.h
+++ src/Player.h
@@ -1,25 +1,23 @@
+/* SEP ASSIGNMENT 2
+Programmed by James Cookson */
+
 #pragma once
+
 #include "Level.h"
 #include "Node.h"
 
 using namespace std;
 
-enum move { NONE, UP, DOWN, LEFT, RIGHT };
-
 class Player
 {
 private:
-	Level F; //Pointer to FileHandler class
-	Node S, startPoint; //Pointers to Node class
-	int length; //Length of current path
-	enum move myMove;
+	Level L; //Pointer to Level class
 
 public:
+	Node playerPoint; //Local node variable created to contain startPoint data from level class
 
 	Player(); //Constructor
 	~Player(); //Deconstructor
 
-	void setMove(enum move);
-	void Direction(); //Runs Check() function for each of the four directions
-	void Check(int, int); //Checks to see next possible nodes to travel to
+	void Check(int, int); //Checks to see if player can move in the chosen direction
 };
