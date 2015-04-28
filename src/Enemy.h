0a28679accb7d190e7bb1e00c392a2ffea945441
James <Cook-E@users.noreply.github.com>
2015-04-28 06:37:40 +0100

    Create Enemy.h

---
+++ src/Enemy.h
@@ -0,0 +1,31 @@
+/* SEP ASSIGNMENT 2
+Programmed by James Cookson */
+
+#pragma once
+
+#include "Level.h"
+#include "Node.h"
+
+class Enemy
+{
+private:
+	Level L; //Pointer to Level class
+	Node N, *currentPathTemp, *currentPath; //Initialise Node class, temporary path storage, path storage
+	int length; //Length of current path to player; //Pointer to Level class
+	int pathSize; //variable used to contain size of the final path
+
+public:
+	int shortestPath; //Shortest path to player
+	bool reached = false; //Checks to see if the path will reach the player before it is stored
+	Node enemyPoint; //Local variable to store enemy co-ordinates
+
+	Enemy(); //Constructor
+	~Enemy(); //Deconstructor
+
+	void recordPath(); //Records the path the enemy has found to player
+	void Direction(Node); //Checks the pathfinding algorithm in 4 directions to see which direction enemy should move next
+	void Check(int, int, Node); //Pathfinding algorithm
+	void PrintPathCoordinates(); //Prints the path co-ordinates the enemy found
+	void setNewPos(); //Updates enemy position
+};
+
